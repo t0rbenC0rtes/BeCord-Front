0345f6b92c2d42c0e5749698122e1d4354421054
Antoine Lafontaine <antoinelaf@gmail.com>
2025-01-28 16:51:04 +0100

    added toggle button

--- src/components/ServerList.jsx
+++ src/components/ServerList.jsx
@@ -4,28 +4,36 @@ import { HiMiniServer } from "react-icons/hi2";
 
 const ServerList = () => {
   return (
-    <div className="serverList">
-      <div className="serverDiv">
-        <MdAdd className="addServerBtn" />
-        <p>add server</p>
+    <>
+      <div className="serverList">
+        <div className="toggle">
+          <label className="switch">
+            <input type="checkbox" />
+            <span className="slider"></span>
+          </label>
+        </div>
+        <div className="serverDiv">
+          <MdAdd className="addServerBtn" />
+          <p>add server</p>
+        </div>
+        <div className="serverDiv">
+          <HiMiniServer className="serverBtn" />
+          <p>server 1</p>
+        </div>
+        <div className="serverDiv">
+          <HiMiniServer className="serverBtn" />
+          <p>server 2</p>
+        </div>
+        <div className="serverDiv">
+          <HiMiniServer className="serverBtn" />
+          <p>server 3</p>
+        </div>
+        <div className="serverDiv">
+          <HiMiniServer className="serverBtn" />
+          <p>server 4</p>
+        </div>
       </div>
-      <div className="serverDiv">
-        <HiMiniServer className="serverBtn" />
-        <p>server 1</p>
-      </div>
-      <div className="serverDiv">
-        <HiMiniServer className="serverBtn" />
-        <p>server 2</p>
-      </div>
-      <div className="serverDiv">
-        <HiMiniServer className="serverBtn" />
-        <p>server 3</p>
-      </div>
-      <div className="serverDiv">
-        <HiMiniServer className="serverBtn" />
-        <p>server 4</p>
-      </div>
-    </div>
+    </>
   );
 };
 
