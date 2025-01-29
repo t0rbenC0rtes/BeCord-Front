475b2b5f3779a44fc4ec3b09fbfe8a76e6b3765c
Antoine L <162304112+t0rbenC0rtes@users.noreply.github.com>
2025-01-29 14:58:50 +0100

    added arrow icon inside textInput

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -1,17 +1,20 @@
 import React from "react";
+import { IoMdSend } from "react-icons/io";
 
 const ChatBox = () => {
   return (
-    <>
-      <div className="lobbyDiv">
-        <div className="lobbyMsg"></div>
+    <div className="lobbyDiv">
+      <div className="lobbyMsg"></div>
+      
+      <div className="inputContainer">
         <input
           type="text"
-          placeholder="Type your message..."
+          placeholder="Type..."
           className="textInput"
         />
+        <IoMdSend className="sendIcon" />
       </div>
-    </>
+    </div>
   );
 };
 
